e8826041190adc97ad22a25209afee5a77190f5a
ingdifilippo <davidedifilippo@iisdaostalaquila.it>
2024-10-10 06:47:31 +0200

    Create positivo.cpp

---
+++ positivo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+
+	cout<<"Inserire un numero:\n";
+	cin>>numero;
+
+	/******************************************************
+	 * si controlla se il numero è positivo               *
+	 *                                                    *
+	 * if (condizione) {istruzione 1} else {istruzione2}; *
+	 *                                                    *
+	 ******************************************************/
+
+	if(numero>0)
+		cout<<"Il numumero inserito è positivo\n";
+	else
+		cout<<"Il numumero inserito è negativo\n";
+
+
+}
