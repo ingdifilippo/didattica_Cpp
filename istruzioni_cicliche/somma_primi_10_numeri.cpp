db803bcdd4fb82652a2d9d02350101a17f6c2a3d
ingdifilippo <davidedifilippo@iisdaostalaquila.it>
2024-12-16 17:59:43 +0100

    Create somma_primi_10_numeri.cpp

---
+++ istruzioni_cicliche/somma_primi_10_numeri.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numero = 0;
+    int i = 1;
+
+
+    cout << "Stampa dei primi 10 numeri!"<< endl;
+
+    while (i <= 10)
+    {
+        cout << i <<endl;
+        i++;
+    }
+
+
+    return 0;
+}
