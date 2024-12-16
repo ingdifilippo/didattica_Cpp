3c7de9e30fb84b04d168282ea462c36591649dba
ingdifilippo <davidedifilippo@iisdaostalaquila.it>
2024-12-16 17:56:49 +0100

    Create ciclo_infinito.cpp

---
+++ istruzioni_cicliche/ciclo_infinito.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+  
+	/********************************************************
+	 * si continua l'esecuzione delle istruzioni tra graffe *
+	 * fino a quando resta vera la condizione di test tra   *
+	 * parentesi tonde:                 										*
+   * Essendo sempre vera la condizione il ciclo           *
+   * si ripete all'infinito                               *
+	 ********************************************************/
+
+	while(true)
+	{
+		cout<<"Ciao ";
+		
+	}
+
+ return 0;
+}
