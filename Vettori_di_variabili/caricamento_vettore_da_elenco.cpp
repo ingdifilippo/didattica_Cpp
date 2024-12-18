f73d9822b6427f69f25f0148d6e085cdf9a16cd8
ingdifilippo <davidedifilippo@iisdaostalaquila.it>
2024-12-18 16:23:25 +0100

    Create caricamento_vettore_da_elenco.cpp

---
+++ Vettori_di_variabili/caricamento_vettore_da_elenco.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int main(void) {
+
+	//Chiedo al sistema si riservare in memoria un blocco di 5 variabili indicizzate di tipo int
+
+	float temperatura[5] = {12.5, 11.3, 11.7, 10.9, 12.4};
+
+	//inizializzo l'indice discorrimento a 0
+	
+	int i = 0;
+
+
+
+	//Uso l'indice per scorrere le variabili in memoria 
+
+	while (i < 5) {
+		cout << "temperatura= " << temperatura[i] << endl;
+		i++;
+	}
+
+	//il precedente ciclo viene eseguito 5 volte, per i = 0, i = 1. i = 2, i = 3, i = 4 
+    //la condizione (i<5) è sempre vera fino a quando i<5
+	return 0;
+}
