3bce6bc90eefdf2ff8f6e96fd70f250bfaa4181a
ingdifilippo <davidedifilippo@iisdaostalaquila.it>
2025-01-23 12:11:57 +0100

    Update stringhe_3_erase.cpp
    Inserimento del primo codice funzionante.

--- stringhe/stringhe_3_erase.cpp
+++ stringhe/stringhe_3_erase.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    string s = "Ciao!";
+
+    cout << s << endl ;
+    s.erase();
+    cout << s << endl;
+    
+    s = "ciao ciao";
+    cout<<s<<endl;
+    
+}
